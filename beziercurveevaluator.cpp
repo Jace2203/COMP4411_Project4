4836a7f4ec2d677cf4ba96872bf380cd200bf727
Brianching522 <brianching522@gmail.com>
2022-04-24 02:08:19 +0800

    in progress

--- beziercurveevaluator.cpp
+++ beziercurveevaluator.cpp
@@ -10,42 +10,60 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 										 const bool& bWrap) const
 {
 	ptvEvaluatedCurvePts.clear();
-	int iCtrlPtCount = ptvCtrlPts.size();
 
-	//ptvEvaluatedCurvePts.push_back( Point( 0, ptvCtrlPts[0].y ));
+	std::vector<Point> ctrl;
+	ctrl.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
 
-	while (iCtrlPtCount > 3)
+	if (bWrap)
+		ctrl.push_back( Point(ptvCtrlPts[0].x + fAniLength, ptvCtrlPts[0].y) );
+
+	float bound;
+	bool used = 0;
+
+	int iCtrlPtCount = ctrl.size();
+
+	if (iCtrlPtCount <= 3)
+		ptvEvaluatedCurvePts.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
+	else
 	{
-		Mat4f Q( -1, +3, -3, +1,
-				 +3, -6, +3, +0,
-				 -3, +3, +0, +0, 
-				 +1, +0, +0, +0);
-		Vec4f xx( ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 0].x, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 1].x, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 2].x, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 3].x);
-		Vec4f yy( ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 0].y, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 1].y, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 2].y, ptvCtrlPts[ptvCtrlPts.size() - iCtrlPtCount + 3].y);
-
-		for(float t = 0; t <= 1; t += 0.01)
+		while (iCtrlPtCount > 3)
 		{
-			Vec4f T( pow(t, 3), pow(t, 2), t, 1);
-			Vec4f res(T * Q);
-			ptvEvaluatedCurvePts.push_back( Point( res * xx, res * yy ));
+			Mat4f Q( -1, +3, -3, +1,
+					 +3, -6, +3, +0,
+					 -3, +3, +0, +0, 
+					 +1, +0, +0, +0);
+			Vec4f xx( ctrl[ctrl.size() - iCtrlPtCount + 0].x, ctrl[ctrl.size() - iCtrlPtCount + 1].x, ctrl[ctrl.size() - iCtrlPtCount + 2].x, ctrl[ctrl.size() - iCtrlPtCount + 3].x);
+			Vec4f yy( ctrl[ctrl.size() - iCtrlPtCount + 0].y, ctrl[ctrl.size() - iCtrlPtCount + 1].y, ctrl[ctrl.size() - iCtrlPtCount + 2].y, ctrl[ctrl.size() - iCtrlPtCount + 3].y);
+
+			for(float t = 0; t < 1; t += 0.01)
+			{
+				Vec4f T( pow(t, 3), pow(t, 2), t, 1);
+				Vec4f res(T * Q);
+				if (res * xx <= fAniLength)
+				{
+					bound = res * yy;
+					used = 1;
+					ptvEvaluatedCurvePts.push_back( Point( res * xx, res * yy ) );
+				}
+				else
+					ptvEvaluatedCurvePts.push_back( Point( res * xx - fAniLength, res * yy ) );
+			}
+			iCtrlPtCount -= 3;
 		}
-		iCtrlPtCount -= 3;
+		while(--iCtrlPtCount >= 0)
+			ptvEvaluatedCurvePts.push_back(ctrl[ctrl.size() - iCtrlPtCount - 1]);
 	}
-
-	for(int i = ptvCtrlPts.size() - iCtrlPtCount; i < ptvCtrlPts.size(); ++i)
-		ptvEvaluatedCurvePts.push_back(ptvCtrlPts[i]);
-
-	iCtrlPtCount = ptvCtrlPts.size();
 
 	float x = 0.0;
 	float y1;
 
-	if (bWrap) {
-		// if wrapping is on, interpolate the y value at xmin and
-		// xmax so that the slopes of the lines adjacent to the
-		// wraparound are equal.
+	iCtrlPtCount = ptvCtrlPts.size();
 
-		if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
+	if (bWrap)
+	{
+		if (used)
+			y1 = bound;
+		else if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
 			y1 = (ptvCtrlPts[0].y * (fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x) + 
 				  ptvCtrlPts[iCtrlPtCount - 1].y * ptvCtrlPts[0].x) /
 				 (ptvCtrlPts[0].x + fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x);
@@ -53,22 +71,21 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 		else 
 			y1 = ptvCtrlPts[0].y;
 	}
-	else {
-		// if wrapping is off, make the first and last segments of
-		// the curve horizontal.
-
+	else
 		y1 = ptvCtrlPts[0].y;
-    }
 
 	ptvEvaluatedCurvePts.push_back(Point(x, y1));
 
-	/// set the endpoint based on the wrap flag.
 	float y2;
     x = fAniLength;
-    if (bWrap)
+    if (bWrap && )
+	{
 		y2 = y1;
-    else
+		ptvEvaluatedCurvePts.push_back(Point(x, y2));
+	}
+    else if (!bWrap)
+	{
 		y2 = ptvCtrlPts[iCtrlPtCount - 1].y;
-
-	ptvEvaluatedCurvePts.push_back(Point(x, y2));
+		ptvEvaluatedCurvePts.push_back(Point(x, y2));
+	}
 }
--- bsplinecurveevaluator.cpp
+++ bsplinecurveevaluator.cpp
@@ -10,19 +10,63 @@ void BSplineCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 {
     ptvEvaluatedCurvePts.clear();
 
-    std::vector<Point> poor;
-    poor.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
-    poor.insert(poor.begin(), 2, ptvCtrlPts[0]);
-    poor.insert(poor.end(), 2, ptvCtrlPts[ptvCtrlPts.size() - 1]);
+    std::vector<Point> boor;
+    boor.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
+    // boor.insert(boor.begin(), 2, ptvCtrlPts[0]);
+    // boor.insert(boor.end(), 2, ptvCtrlPts[ptvCtrlPts.size() - 1]);
 
-    for(int i = 0; i < poor.size() - 3; ++i)
+	// while (boor.size() < 4)
+	// 	boor.push_back( Point(0, 0) );
+
+    int iCtrlPtCount = ptvCtrlPts.size();
+
+	float x = 0.0;
+	float y1;
+
+	if (bWrap)
+	{
+		// if wrapping is on, interpolate the y value at xmin and
+		// xmax so that the slopes of the lines adjacent to the
+		// wraparound are equal.
+
+		if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
+			y1 = (ptvCtrlPts[0].y * (fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x) + 
+				  ptvCtrlPts[iCtrlPtCount - 1].y * ptvCtrlPts[0].x) /
+				 (ptvCtrlPts[0].x + fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x);
+		}
+		else 
+			y1 = ptvCtrlPts[0].y;
+
+		// boor.insert(boor.begin(), Point( boor[boor.size() - 1].x - fAniLength, boor[boor.size() - 1].y) );
+		// boor.insert(boor.end(), Point( boor[1].x + fAniLength, boor[1].y) );
+	}
+	else {
+		// if wrapping is off, make the first and last segments of
+		// the curve horizontal.
+
+		y1 = ptvCtrlPts[0].y;
+    }
+
+	//ptvEvaluatedCurvePts.push_back(Point(x, y1));
+
+	/// set the endpoint based on the wrap flag.
+	float y2;
+    x = fAniLength;
+    if (bWrap)
+		y2 = y1;
+    else
+		y2 = ptvCtrlPts[iCtrlPtCount - 1].y;
+
+	//ptvEvaluatedCurvePts.push_back(Point(x, y2));
+
+	for(int i = 0; i < boor.size() - 3; ++i)
 	{
 		Mat4f Q( -1, +3, -3, +1,
 				 +3, -6, +3, +0,
 				 -3, +0, +3, +0, 
 				 +1, +4, +1, +0);
-		Vec4f xx( poor[i + 0].x, poor[i + 1].x, poor[i + 2].x, poor[i + 3].x);
-		Vec4f yy( poor[i + 0].y, poor[i + 1].y, poor[i + 2].y, poor[i + 3].y);
+		Vec4f xx( boor[i + 0].x, boor[i + 1].x, boor[i + 2].x, boor[i + 3].x);
+		Vec4f yy( boor[i + 0].y, boor[i + 1].y, boor[i + 2].y, boor[i + 3].y);
 
 		for(float t = 0; t <= 1; t += 0.01)
 		{
@@ -31,41 +75,4 @@ void BSplineCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 			ptvEvaluatedCurvePts.push_back( Point( res * xx, res * yy ));
 		}
 	}
-
-    int iCtrlPtCount = poor.size();
-
-	float x = 0.0;
-	float y1;
-
-	if (bWrap) {
-		// if wrapping is on, interpolate the y value at xmin and
-		// xmax so that the slopes of the lines adjacent to the
-		// wraparound are equal.
-
-		if ((poor[0].x + fAniLength) - poor[iCtrlPtCount - 1].x > 0.0f) {
-			y1 = (poor[0].y * (fAniLength - poor[iCtrlPtCount - 1].x) + 
-				  poor[iCtrlPtCount - 1].y * poor[0].x) /
-				 (poor[0].x + fAniLength - poor[iCtrlPtCount - 1].x);
-		}
-		else 
-			y1 = ptvCtrlPts[0].y;
-	}
-	else {
-		// if wrapping is off, make the first and last segments of
-		// the curve horizontal.
-
-		y1 = poor[0].y;
-    }
-
-	ptvEvaluatedCurvePts.push_back(Point(x, y1));
-
-	/// set the endpoint based on the wrap flag.
-	float y2;
-    x = fAniLength;
-    if (bWrap)
-		y2 = y1;
-    else
-		y2 = poor[iCtrlPtCount - 1].y;
-
-	ptvEvaluatedCurvePts.push_back(Point(x, y2));
 }
